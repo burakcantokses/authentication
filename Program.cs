fe7d4647ca9d22ceff78043f3a416d02b484506c
Burakcan TOKSES <75482610+burakcantokses@users.noreply.github.com>
2021-02-03 20:34:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delta_Authenticaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string id,username,pass,finished;
+            System.Console.WriteLine("ID'nizi Girin.");
+            id = Console.ReadLine();
+
+            System.Console.WriteLine("Kullanıcı Adınızı Girin.");
+            username = Console.ReadLine();
+
+            System.Console.WriteLine("Şifrenizi Girin.");
+            pass = Console.ReadLine();
+            
+            finished = username+":"+pass;
+
+            string webpageData;
+            using (System.Net.WebClient webClient = new System.Net.WebClient())
+            webpageData = webClient.DownloadString("https://raw.githubusercontent.com/burakcantokses/authentication/main/user"+id);
+            var webpage = webpageData;
+            if (webpage.Contains(finished)) {
+                System.Console.WriteLine("Başarılı!");
+            }else {
+                System.Console.WriteLine("başarısız");
+            }
+            System.Console.WriteLine(finished);
+            System.Console.WriteLine(webpage);
+
+        }
+    }
+}
